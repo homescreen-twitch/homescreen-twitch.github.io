a94a9763e1b7b276b71f3a188f0cc4698d6e1a36
homescreen-twitch <homescreen-twitch@asd.de>
2020-05-07 18:45:01 +0200

    add oauth page

---
+++ oauth.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    arr = window.location.hash.split("&")
+    .map(v => v.split("=")).reduce( (pre, [key, value]) => ({ ...pre, [key]: value }), {} );
+    console.log(arr['#access_token']);
+    window.localStorage.setItem('oauth', arr['#access_token']);
+    
+});
